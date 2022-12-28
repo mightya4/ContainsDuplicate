54e946ddbed330bf4e5283181b69e067f58595d7
Akeem <akeem.mighty@gmail.com>
2022-12-28 00:48:09 -0500

    Completed Contains Duplicate Functionality

--- Solution.java
+++ Solution.java
@@ -1,8 +1,22 @@
+import java.util.HashMap;
 class Solution {
     public static boolean containsDuplicate(int[] nums) {
-        
+        HashMap<Integer, Integer> duplicateHashmap = new HashMap<Integer, Integer>();
+        for(int num : nums){
+            if(duplicateHashmap.containsKey(num)){
+                return true;
+            } else {
+                duplicateHashmap.put(num, 1);
+            }
+        }
+        return false;
     }
     public static void main(String[] args) {
-        
+        int[] testArrayOne = new int[]{1,2,3,1};
+        containsDuplicate(testArrayOne);
+        int[] testArrayTwo = new int[]{1,2,3,4};
+        containsDuplicate(testArrayTwo);
+        int[] testArrayThree = new int[]{1,1,1,3,3,4,3,2,4,2};
+        containsDuplicate(testArrayThree);
     }
 }
